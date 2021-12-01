4abbbf534ff7dfca13682871f05d98081bce7204
Fran <frangal@gmail.com>
2021-12-01 15:11:06 +0100

    check debootstrap

--- rpi-img-builder.sh
+++ rpi-img-builder.sh
@@ -117,8 +117,7 @@ systemd-container debootstrap xz-utils kmod udev dbus gnupg gnupg-utils debian-a
 installdeps
 
 # Checkear versión mínima debootstrap
-DEBOOTSTRAP_VER=$(debootstrap --version | grep -o '[0-9.]\+' | head -1)
-if dpkg --compare-versions "$DEBOOTSTRAP_VER" lt "1.0.105"; then
+if dpkg --compare-versions "$(dpkg-query -f '${Version}' -W debootstrap)" lt "1.0.105"; then
   echo "Actualmente su versión de debootstrap no soporta el script" >&2
   echo "Actualice debootstrap, versión mínima 1.0.105" >&2
   exit 1
